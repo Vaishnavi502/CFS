b8ed6fd3f65814cd07b4ee67efc3a4a71b38454d
Vaishnavi502 <68768878+Vaishnavi502@users.noreply.github.com>
2022-11-13 15:37:37 +0000

    Check if client has entered correct pass key

---
+++ passkey.py
@@ -0,0 +1,19 @@
+# To check if client has entered correct pass key
+import os
+from subprocess import call
+DEFAULT_KEY_FILE="./mykey.pem"
+RSA_KEY_SIZE=2048
+
+def gen_keys():
+    key_file=DEFAULT_KEY_FILE
+    key_pub_file=f"{key_file}.pub"
+    # Generate RSA key pair
+    call(['openssl','genrsa','-out',key_file,str(RSA_KEY_SIZE)])
+    # Extract public key
+    with open(key_pub_file,'w') as stdout:
+        call(['openssl','rsa','-in',key_file,'-pubout'],stdout=stdout)
+    # Broadcast RSA public key
+    with open(key_pub_file,'r') as fin:
+        print(fin.read())
+
+def check(key):
