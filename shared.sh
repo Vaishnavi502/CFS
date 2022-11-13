2512f474d3f4533618c86d97d3616964c0c06b7b
Vaishnavi502 <68768878+Vaishnavi502@users.noreply.github.com>
2022-11-13 14:25:34 +0000

    Commands to run for python package installation

--- shared.sh
+++ shared.sh
@@ -2,3 +2,7 @@
 apt install samba smbclient
 systemctl status smbd
 ufw allow 'Samba'
+
+#pip3 install cryptography
+#python -m pip install --upgrade pip
+#export PATH="usr/local/python/3.10.4/bin:$PATH"
