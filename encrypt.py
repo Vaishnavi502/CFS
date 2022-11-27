0cd9c7e000943f220663bbb194b3d44c7b5e1fd3
Vaishnavi502 <68768878+Vaishnavi502@users.noreply.github.com>
2022-11-27 12:54:15 +0000

    2 level encryption of all text files

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,6 @@
 import os
 from cryptography.fernet import Fernet
-# import passkey
+import encrypt2
 # import decrypt
 
 # Create file to store generated key ?
@@ -9,6 +9,7 @@ os.chdir(dirpath)
 origkey = Fernet.generate_key()
 
 key = Fernet(origkey)
+encrypt2.gen_keys()
 for file in os.listdir():
     if file.endswith(".txt"):
         fpath = f"{dirpath}/{file}"
@@ -16,13 +17,14 @@ for file in os.listdir():
             orig = f.read()
         # Encrypt file
         encrypted = key.encrypt(orig)
+        encrypted2 = encrypt2.re_encrypt(encrypted)
         # Copy file into subdirectory ./output
         op = f"{dirpath}/output/"
         if not os.path.exists(op):
             os.makedirs(op)
         opfile = f"{op}/{file}"
         with open(opfile,'wb') as f:
-            f.write(encrypted)
+            f.write(encrypted2)
 
 def genkey():
-    return key,opfile
+    return key
