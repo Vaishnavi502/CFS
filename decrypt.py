44fee78545cc062835ce9caa6b87380fb928011c
Vaishnavi502 <68768878+Vaishnavi502@users.noreply.github.com>
2022-11-27 12:53:22 +0000

    Decryption using RSA and symmetric methods

--- decrypt.py
+++ decrypt.py
@@ -1,9 +1,18 @@
-import encrypt
+import encrypt,encrypt2
+import os
 
-dkey,dpath=encrypt.genkey()
+dirpath = "/workspaces/CFS/files"
+os.chdir(dirpath)
 
-with open(dpath,'rb') as f:
-    encrypted=f.read()
+dkey=encrypt.genkey()
 
-decrypted=dkey.decrypt(encrypted)
-print(decrypted.decode('utf8'))
+for file in os.listdir():
+    if file.endswith(".txt"):
+        dpath=f'{dirpath}/output/{file}'
+        with open(dpath,'rb') as f:
+            encrypted=f.read()
+        print(f'\nContent of {file} is:')
+        decrypted2=encrypt2.re_decrypt(encrypted)
+        decrypted=dkey.decrypt(decrypted2)
+        st=decrypted.decode('utf8')
+        print(st)
