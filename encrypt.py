15f0213407edaaeab0bf68d3978e53bb5f7405ce
Vaishnavi502 <68768878+Vaishnavi502@users.noreply.github.com>
2022-11-27 05:42:10 +0000

    Encrypt all files in files subdirectory

--- encrypt.py
+++ encrypt.py
@@ -4,20 +4,21 @@ from cryptography.fernet import Fernet
 # import decrypt
 key = Fernet.generate_key()
 # Create file to store generated key ?
-dpath = "/workspaces/CFS/files"
-os.chdir(dpath)
+dirpath = "/workspaces/CFS/files"
+os.chdir(dirpath)
 
 key = Fernet(key)
 for file in os.listdir():
     if file.endswith(".txt"):
-        fpath = f"{dpath}/{file}"
+        fpath = f"{dirpath}/{file}"
         with open(fpath, 'rb') as f:
             orig = f.read()
         # Encrypt file
         encrypted = key.encrypt(orig)
         # Copy file into subdirectory ./output
-        op = f"{dpath}/output/{file}"
+        op = f"{dirpath}/output/"
         if not os.path.exists(op):
-                os.makedirs(op)
-        with open(op,'wb') as f:
+            os.makedirs(op)
+        opfile = f"{op}/{file}"
+        with open(opfile,'wb') as f:
             f.write(encrypted)
